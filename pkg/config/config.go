1c3e83081bbcc271c97d063e1167b9f151f85023
Rich Lander <lander2k2@protonmail.com>
2023-09-01 11:20:15 -0400

    feat: add AWS config load with static creds
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 )
 
 // LoadAWSConfig loads the AWS config from environment or shared config profile
@@ -18,6 +19,28 @@ func LoadAwsConfig(configProfile, region string) (*aws.Config, error) {
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load AWS config: %w", err)
+	}
+
+	return &awsConfig, err
+}
+
+// LoadAWSConfigFromAPIKeys returns an AWS config from static API keys and
+// overrides the default region if provided.  The token parameter can be an
+// empty string.
+func LoadAWSConfigFromAPIKeys(accessKeyID, secretAccessKey, token, region string) (*aws.Config, error) {
+	awsConfig, err := config.LoadDefaultConfig(
+		context.Background(),
+		config.WithRegion(region),
+		config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				accessKeyID,
+				secretAccessKey,
+				token,
+			),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config from static API keys: %w", err)
 	}
 
 	return &awsConfig, err
